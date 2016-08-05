e8dae248893df79c8f439dd34c0d016cf088544d
vitek999 <vitek2012rus@gmail.com>
2016-08-05 09:08:01 +0300

    add answerOn and getAnswer functions

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import requests
 import json
+import urllib.parse
 import bot_config
 
-
 def getMessages():
-	response = requests.get('http://{}?act=a_check&key={}&ts={}&wait=50&mode=1'.format(server, key, ts))
+	response = requests.get('http://{}?act=a_check&key={}&ts={}&wait=25&mode=1'.format(server, key, ts))
 	res = response.json()
 	global ts
 	ts = res['ts']
@@ -15,6 +15,7 @@ def getMessages():
 		 	b.update({'cid': int(a[3] - 2E9)})
 		else:
 			b.update({'uid': a[3]})
+		answerOn(b)
 		print(b)
 	getMessages()
 
@@ -31,7 +32,19 @@ def getLongPollServer():
 	ts = parsed_string['response']['ts']
 	getMessages()
 
+def answerOn(msg):
+	def answer(answer):
+		answer = urllib.parse.quote(answer, safe='~()*!.\'')
+		if 'cid' in msg:
+			print('answering in chat:', answer)
+			response = requests.get('https://api.vk.com/method/messages.send?chat_id={}&message={}&access_token={}'.format(msg['cid'], answer, bot_config.vk_token))
+		else:
+			print('answering to user:', answer)
+			response = requests.get('https://api.vk.com/method/messages.send?user_id={}&message={}&access_token={}'.format(msg['uid'], answer, bot_config.vk_token))
+	getAnswer(msg['msg'], answer)
 
-
+def getAnswer(msg, answer):
+	if msg == 'test':
+		answer('test succes!')
 
 getLongPollServer()
