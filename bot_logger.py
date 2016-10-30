377678cdecf6f268d870948f73950694d8b4b594
F6CF <Creeplays@users.noreply.github.com>
2016-10-30 23:16:25 +0500

    Because not all terminals supports colors.
    Also check out my bot, i will be very thankful if you porn Ayzek SDK to python after BETA

--- bot_logger.py
+++ bot_logger.py
@@ -1,5 +1,9 @@
 from datetime import datetime, date, time
+import sys
 
 def printLog(module_name, log_msg):
     dt = datetime.now().strftime("%H:%M:%S")
-    print('\x1b[33m['+ dt +']\x1b[32m['+module_name+']\x1b[0m: ' + str(log_msg))
+    if sys.stdout.isatty():
+        print('\x1b[33m['+ dt +']\x1b[32m['+module_name+']\x1b[0m: ' + str(log_msg))
+    else:
+        print('['+ dt +']['+module_name+']: ' + str(log_msg))
